58400f2c7e3ac97fbb06fac5678a8f1c42b67fae
egoisma <semir0228@gmail.com>
2020-03-07 01:48:26 +0900

    add blank line

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 @app.route("/")
+
 def hello():
 	return "<font size=7>Hello</font>"
+
 if __name__=="__main__":
 	app.run(host="localhost", port="80")
