debaea6c5a9f92cdc61b1ef0ffc9d7df8e3bf313
egoisma <semir0228@gmail.com>
2020-03-29 16:08:15 +0900

    add comment

--- test.py
+++ test.py
@@ -5,26 +5,34 @@ import pytz
 import threading
 
 end=False
+sec=0;
 def crawl(second=1.0):
-    global end
+    global end, sec
     if end:
         return
+    
     now = datetime.now(pytz.timezone('UTC'))
-    URL = 'https://namu.wiki/RecentChanges'
-    res = requests.get(URL)
+    
+    if sec != now.strftime('%S'):
+        sec = now.strftime('%S')
 
-    print('status:', res.status_code, now.strftime('%Y-%m-%d %H:%M:%S')[11:])
-    html = res.text
-    soup = BeautifulSoup(html, 'lxml')
+        URL = 'https://namu.wiki/RecentChanges'
+        res = requests.get(URL)
 
-    body = soup.body
+        print('status:', res.status_code, now.strftime('%Y-%m-%d %H:%M:%S')[11:])
+        html = res.text
+        soup = BeautifulSoup(html, 'lxml')
 
-    for tr in body.article.tbody.find_all('tr'):
-        if(tr.td.a):
-            if now.strftime('%Y-%m-%d %H:%M:%S')[8:] == tr.find_all('td')[2].time.text[8:]:
-                print(tr.td.a.text)
+        body = soup.body
+        #if(body.article.tbody.find_all('tr')[0].td.a):
+        #    print('0->', body.article.tbody.find_all('tr')[0].td.a.text, body.article.tbody.find_all('tr')[0].find_all('td')[2].time.text[8:])
+        for tr in body.article.tbody.find_all('tr'):
+            if(tr.td.a):
+                # 데이터 업데이트 후 크롤링할 때 1초씩 늦어서 시간 매칭을 못 한다.
+                if now.strftime('%Y-%m-%d %H:%M:%S')[8:] == tr.find_all('td')[2].time.text[8:]:
+                    print('->', tr.td.a.text)
 
     threading.Timer(second, crawl, [second]).start()
-    print()
 
-crawl(0.9)
+# 
+crawl(0.5)
