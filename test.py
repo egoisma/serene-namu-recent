e30e381f0d01fec6b816dfb8e64dd6b32b1426ae
egoisma <semir0228@gmail.com>
2020-03-29 12:56:43 +0900

    implement test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import pytz
 import threading
+
 end=False
 def crawl(second=1.0):
     global end
@@ -12,8 +13,7 @@ def crawl(second=1.0):
     URL = 'https://namu.wiki/RecentChanges'
     res = requests.get(URL)
 
-    print(res.status_code)
-    print()
+    print('status:', res.status_code, now.strftime('%Y-%m-%d %H:%M:%S')[11:])
     html = res.text
     soup = BeautifulSoup(html, 'lxml')
 
@@ -22,8 +22,9 @@ def crawl(second=1.0):
     for tr in body.article.tbody.find_all('tr'):
         if(tr.td.a):
             if now.strftime('%Y-%m-%d %H:%M:%S')[8:] == tr.find_all('td')[2].time.text[8:]:
-                print(tr.td.a.text, tr.find_all('td')[2].time.text[8:])
+                print(tr.td.a.text)
 
     threading.Timer(second, crawl, [second]).start()
+    print()
 
-crawl(1)
+crawl(0.9)
