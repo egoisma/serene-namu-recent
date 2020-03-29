aed40465cce8b251e67be661c142de77af02035a
egoisma <semir0228@gmail.com>
2020-03-29 20:29:21 +0900

    implement test.py

--- test.py
+++ test.py
@@ -28,10 +28,13 @@ def crawl(second=1.0):
         #    print('0->', body.article.tbody.find_all('tr')[0].td.a.text, body.article.tbody.find_all('tr')[0].find_all('td')[2].time.text[8:])
         for tr in body.article.tbody.find_all('tr'):
             if(tr.td.a):
-                # 데이터 업데이트 후 크롤링할 때 1초씩 늦어서 시간 매칭을 못 한다.
-                if now.strftime('%Y-%m-%d %H:%M:%S')[8:] == tr.find_all('td')[2].time.text[8:]:
-                    print('->', tr.td.a.text)
-
+                # 데이터 업데이트 후 크롤링 시 1초씩 늦어서 시간 매칭을 못 한다.
+                # -> 현재 시간의 1~2초 전 데이터를 가져온다.
+                time_text = tr.find_all('td')[2].time.text
+                if now.strftime('%Y-%m-%d %H:%M')[8:] == time_text[8:-3]:
+                    if int(now.strftime('%S'))-2 == int(time_text[-2:]):
+                        print('->', tr.td.a.text, ',', int(tr.td.span.text[1:-1]))
+        print()
     threading.Timer(second, crawl, [second]).start()
 
 # 
